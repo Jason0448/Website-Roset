d265d71ce0989031cdaf6eb422b5b5a41d5b784f
Jason <jason.jpy.wong@gmail.com>
2022-10-11 13:30:48 +0200

    Database Connectie Test

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+
+// Database configuratie
+$host  = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "roset-db";
+ 
+// Maak een  database connectie
+$conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
+
+// Controleer de verbinding
+if(mysqli_connect_error())
+{
+ echo "Connection establishing failed!";
+}
+else
+{
+ echo "Connection established successfully.";
+}
+
+?>
